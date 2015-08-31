3f9d1db57c85cad93982e915e6c9fac597e1e13c
Whimsy Elysium Duke <whimsyduke@hotmail.com>
2015-08-31 13:57:07 +0800

    2015.8.31修正使用 GetValueMForAALM()替代switch

--- ns-3/src/pmtmgmp/model/my11s/pmtmgmp-protocol.cc
+++ ns-3/src/pmtmgmp/model/my11s/pmtmgmp-protocol.cc
@@ -1639,23 +1639,8 @@ namespace ns3 {
 
 			////PGEN信息更新
 			pathCopy->DecrementTtl();
-			double magnification;
-			switch (m_NodeType)
-			{
-			case Mesh_Access_Point:
-			case Mesh_Portal:
-				magnification = m_PMTMGMPmterpAALMmagnification;
-				break;
-			case Mesh_Secondary_Point:
-				magnification = m_PMTMGMPmsecpAALMmagnification;
-				break;
-			case Mesh_STA:
-				magnification = 1;
-				break;
-			default:
-				NS_LOG_ERROR("Error node type.");
-			}
-			pathCopy->IncrementMetric(metric, magnification);
+			
+			pathCopy->IncrementMetric(metric, GetValueMForAALM());
 
 			pathCopy->SetFromNode(from);
 			pathCopy->SetPGENsendTime();
@@ -1744,23 +1729,8 @@ namespace ns3 {
 
 			////PGEN信息更新
 			pathCopy->DecrementTtl();
-			double magnification;
-			switch (m_NodeType)
-			{
-			case Mesh_Access_Point:
-			case Mesh_Portal:
-				magnification = m_PMTMGMPmterpAALMmagnification;
-				break;
-			case Mesh_Secondary_Point:
-				magnification = m_PMTMGMPmsecpAALMmagnification;
-				break;
-			case Mesh_STA:
-				magnification = 1;
-				break;
-			default:
-				NS_LOG_ERROR("Error node type.");
-			}
-			pathCopy->IncrementMetric(metric, magnification);
+			
+			pathCopy->IncrementMetric(metric, GetValueMForAALM());
 
 			pathCopy->SetFromNode(from);
 			pathCopy->SetPGENsendTime();
@@ -1926,22 +1896,6 @@ namespace ns3 {
 			std::vector<PUPGQdata> recreateList;
 			std::vector<PUPDaalmTreeData> data = pupd.GetData();
 			////AALM更新参数设置
-			double magnification;
-			switch (m_NodeType)
-			{
-			case Mesh_Access_Point:
-			case Mesh_Portal:
-				magnification = m_PMTMGMPmterpAALMmagnification;
-				break;
-			case Mesh_Secondary_Point:
-				magnification = m_PMTMGMPmsecpAALMmagnification;
-				break;
-			case Mesh_STA:
-				magnification = 1;
-				break;
-			default:
-				NS_LOG_ERROR("Error node type.");
-			}
 			NS_LOG_DEBUG("Receive PUPD  from " << from << " at node " << m_address << " at interface " << interface << " while metric is " << metric);
 			for (std::vector<PUPDaalmTreeData>::iterator iter = data.begin(); iter != data.end(); iter++)
 			{
@@ -1963,7 +1917,7 @@ namespace ns3 {
 					{
 						////路由表中存在指定路由路径且来自于PUPD的发送者，更新Metric
 						find->SetMetric(select->GetMetric());
-						find->IncrementMetric(metric, magnification);
+						find->IncrementMetric(metric, GetValueMForAALM());
 						NS_LOG_DEBUG("PUPD  Infor: MTERP(" << mterp << "),M SECPindex(" << uint32_t(select->GetMSECPindex()) << "), Metric(" << find->GetMetric() << ")");
 					}
 					else
